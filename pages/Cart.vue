0be3ac9522c0050058c8c4cdbdf418ea1df40305
yeg2799 <yeg2799@gmail.com>
2021-04-18 12:56:07 +0300

    cart ui fixed

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-      Cart
+  <div class="cart">
+        <div class="title">
+          <h1>Sepetiniz</h1>
+          <p>(2 Ürün)</p>
+        </div>
+        <div class="products">
+          <img src="@/assets/image/products/product_samsung_EA2732_tv.jpg" alt="" class="productImage">
+          <h4>PHILIPS 50PUS8505 50" </h4>
+          <span>Adet:2</span>
+          <span>3900₺</span>
+          <img src="@/assets/icons/svg/multiplication.svg" alt="" class="icon"/>
+        </div>
+        <div class="products">
+          <img src="@/assets/image/products/product_samsung_EA2732_tv.jpg" alt="" class="productImage">
+          <h4>PHILIPS 50PUS8505 50" </h4>
+          <span>Adet:2</span>
+          <span>3900₺</span>
+          <img src="@/assets/icons/svg/multiplication.svg" alt="" class="icon"/>
+        </div>
+        <div class="products">
+          <img src="@/assets/image/products/product_samsung_EA2732_tv.jpg" alt="" class="productImage">
+          <h4>PHILIPS 50PUS8505 50" </h4>
+          <span>Adet:2</span>
+          <span>3900₺</span>
+          <img src="@/assets/icons/svg/multiplication.svg" alt="" class="icon"/>
+        </div>
+        <div class="payment">
+          <span>Toplam:7800₺</span>
+          <div class="button">
+            Ödemeye Geç
+          </div>
+        </div>
   </div>
 </template>
 
@@ -10,6 +40,38 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.cart{
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  font-size: 20px;
+  .title{
+    text-align: center;
+  }
+  .products{
+    display: flex;
+    justify-content: space-evenly;
+    margin-bottom: 70px;
+    .productImage{
+      width: 150px;
+    }
+    .icon{
+      width: 25px;
+    }
+  }
+  .payment{
+    display: flex;
+    position: absolute;
+    align-items: center;
+    right: 220px;
+    bottom:0px ;
 
+    .button{
+      border: 3px solid black;
+      padding: 10px;
+      margin-left: 50px;
+    }
+  }
+}
 </style>
