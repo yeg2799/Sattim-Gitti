d2d0b274fab2643cf8e24a54e9ead0244274cb48
yeg2799 <yeg2799@gmail.com>
2021-06-22 00:00:43 +0300

    for deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 import routers from './config/routes'
 export default {
+  target:'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'SattimGitti',
@@ -48,4 +49,7 @@ export default {
       routes.push(...routers);
     },
   },
+  generate: {
+    fallback: true
+  }
 }
